a38e8b8203e02c2c92def452d06c222d9fcf29f4
Ektiva <ektiva2013@gmail.com>
2020-05-23 02:07:45 -0400

    Set the API Architecture

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -12,18 +13,18 @@ namespace Infrastructure.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
-        //public DbSet<ProductBrand> ProductBrands { get; set; }
-        //public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
         //public DbSet<Order> Orders { get; set; }
         //public DbSet<OrderItem> OrderItems { get; set; }
         //public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
 
-        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            /*if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
             {
                 foreach (var entityType in modelBuilder.Model.GetEntityTypes())
                 {
@@ -42,8 +43,8 @@ namespace Infrastructure.Data
                             .HasConversion(new DateTimeOffsetToBinaryConverter());
                     }
                 }
-            }
-        }*/
+            }*/
+        }
 
     }
 }

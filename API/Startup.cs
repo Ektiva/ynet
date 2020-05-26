ce195759b55dbdd62f8480293f15ebcec370b3c1
Ektiva <ektiva2013@gmail.com>
2020-05-25 21:58:42 -0400

    Creating and SetUp the UI

--- API/Startup.cs
+++ API/Startup.cs
@@ -73,7 +73,8 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4300");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 });
             });
 
